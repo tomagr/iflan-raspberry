2ee5d58021a8f19cdcad708385b32a015f39fe7f
Tomas Agrimbau <agrimbautomas@gmail.com>
2020-09-06 19:51:25 -0300

    Add working sound file

--- logs/helpers/sound.py
+++ logs/helpers/sound.py
@@ -8,17 +8,18 @@ channel = 17
 device_id = 1
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
-PAUSE_TIME = 5000
+PAUSE_TIME = 5000 # 5 Seconds
 
 
 def read_sound():
-    GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=PAUSE_TIME)  # let us know when the pin goes HIGH or LOW
-    GPIO.add_event_callback(channel, callback)
+    GPIO.add_event_detect(channel, GPIO.BOTH, callback=callback, bouncetime=PAUSE_TIME)  # let us know when the pin goes HIGH or LOW
+    # GPIO.add_event_callback(channel, callback)
 
     while True:
-        time.sleep(10)
+        time.sleep(1)
 
 
 def callback(channel):
     response = post_noise_log(device_id)
     print("Sound Detected! Channel: " + str(channel) + " - Response: " + str(response))
+    GPIO.cleanup()
