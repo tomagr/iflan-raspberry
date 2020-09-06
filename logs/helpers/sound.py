254f74c73486d14726c38ed07f7d42a239927d1c
Tomas Agrimbau <tomas@amalgama.co>
2020-09-06 17:42:29 -0300

    Update sounds

--- logs/helpers/sound.py
+++ logs/helpers/sound.py
@@ -12,7 +12,6 @@ GPIO.setup(channel, GPIO.IN)
 def read_sound():
     GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
     GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
-
     while True:
         time.sleep(1)
 
