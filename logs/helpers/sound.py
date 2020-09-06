0d3a76275537d741accec561ed3b0c8d70d9b6cd
Tomas Agrimbau <tomas@amalgama.co>
2020-09-06 17:45:23 -0300

    Refactor logs

--- logs/helpers/sound.py
+++ logs/helpers/sound.py
@@ -9,9 +9,10 @@ device_id = 1
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
+
 def read_sound():
     GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-    GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+    GPIO.add_event_callback(channel, callback)
     while True:
         time.sleep(1)
 
