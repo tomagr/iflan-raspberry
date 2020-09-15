a30afb32d1980ad98f169efe58f836f059b3d358
Tomas Agrimbau <tomas@amalgama.co>
2020-09-15 19:38:11 -0300

    Add tmp hum sensor reader

--- logs/helpers/tmp_hum_sensor.py
+++ logs/helpers/tmp_hum_sensor.py
@@ -1,14 +1,23 @@
+from logs.helpers.iflan_api import post_tmp_hum_log
 import Adafruit_DHT
+import time
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
+DELAY = 10  # Seconds
 
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+def post_tmp_hum(tmp, hum):
+    if hum is not None or tmp is not None:
+        post_tmp_hum_log(tmp, hum)
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(tmp, hum))
+        print(tmp)
+        print(hum)
     else:
         print("Failed to retrieve data from humidity sensor")
 
 
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    post_tmp_hum(temperature, humidity)
+    time.sleep(DELAY)
