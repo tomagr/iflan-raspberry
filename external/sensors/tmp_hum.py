50ca719d219044a0d3f146cfa9acbc4c4056537f
Tomas Agrimbau <tomas@amalgama.co>
2020-09-16 17:34:45 -0300

    Add adafruit library

--- external/sensors/tmp_hum.py
+++ external/sensors/tmp_hum.py
@@ -1,8 +1,18 @@
 from api.api import post_tmp_hum_log
 from config.settings import Settings
+import Adafruit_DHT
 import time
 
 DELAY = 60  # Seconds
+
+
+def get_tmp_hum():
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(Settings.DHT_SENSOR, Settings.DHT_PIN)
+        tmp = round(temperature, 2)
+        hum = round(humidity, 2)
+        post_tmp_hum_log(tmp, hum)
+        time.sleep(DELAY)
 
 
 def read_tmp_hum(tmp, hum):
@@ -11,12 +21,3 @@ def read_tmp_hum(tmp, hum):
         print("Response: " + str(response) + " - Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(tmp, hum))
     else:
         print("Failed to retrieve data from humidity sensor")
-
-
-def get_tmp_hum():
-    while True:
-        humidity, temperature = Settings.Adafruit_DHT.read_retry(Settings.DHT_SENSOR, Settings.DHT_PIN)
-        tmp = round(temperature, 2)
-        hum = round(humidity, 2)
-        post_tmp_hum_log(tmp, hum)
-        time.sleep(DELAY)
