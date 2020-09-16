95f064b36452aa6b63378a421353466e7268a3ed
Tomas Agrimbau <tomas@amalgama.co>
2020-09-16 17:28:34 -0300

    Add tmp and hum to external

--- external/config/settings.py
+++ external/config/settings.py
@@ -1,4 +1,9 @@
+import Adafruit_DHT
+
 class Settings:
     API_URL = 'http://api.iflan.house/1'
     DEVICE_ID = 1
     CHANNEL = 17
+    DHT_PIN = 4
+    DHT_SENSOR = Adafruit_DHT.DHT22
+
--- external/sensors/tmp_hum.py
+++ external/sensors/tmp_hum.py
@@ -1,13 +1,11 @@
-from logs.helpers.iflan_api import post_tmp_hum_log
-import Adafruit_DHT
+from api.api import post_tmp_hum_log
+from config.settings import Settings
 import time
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
 DELAY = 60  # Seconds
 
 
-def post_tmp_hum(tmp, hum):
+def read_tmp_hum(tmp, hum):
     if hum is not None or tmp is not None:
         response = post_tmp_hum_log(tmp, hum)
         print("Response: " + str(response) + " - Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(tmp, hum))
@@ -17,8 +15,8 @@ def post_tmp_hum(tmp, hum):
 
 def get_tmp_hum():
     while True:
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        humidity, temperature = Settings.Adafruit_DHT.read_retry(Settings.DHT_SENSOR, Settings.DHT_PIN)
         tmp = round(temperature, 2)
         hum = round(humidity, 2)
-        post_tmp_hum(tmp, hum)
+        post_tmp_hum_log(tmp, hum)
         time.sleep(DELAY)
