f74703877b6ac7bce506db59ddb6ec9046ddd5af
Tomas Agrimbau <tomas@amalgama.co>
2020-09-16 20:20:43 -0300

    Add callback in spund

--- external/sensors/sound.py
+++ external/sensors/sound.py
@@ -7,24 +7,26 @@ import time
 # GPIO SETUP
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(Settings.CHANNEL, GPIO.IN)
-PAUSE_TIME_MIL = 5000  # 5 Seconds
-PAUSE_TIME_SEG = 5
+PAUSE_TIME_MIL = 1000  # 5 Seconds
+PAUSE_TIME_SEG = 1
 
 
 def read_sound():
-    GPIO.add_event_detect(
-        Settings.CHANNEL,
-        GPIO.BOTH,
-        callback=callback,
-        bouncetime=PAUSE_TIME_MIL
-    )  # let us know when the pin goes HIGH or LOW
-    # GPIO.add_event_callback(channel, callback)
-
-    while True:
-        time.sleep(PAUSE_TIME_SEG)
+	GPIO.add_event_detect(
+		Settings.CHANNEL,
+		GPIO.BOTH,
+		callback=callback,
+		bouncetime=PAUSE_TIME_MIL
+	)  # let us know when the pin goes HIGH or LOW
+	# GPIO.add_event_callback(channel, callback)
+	#
+	while True:
+		print("WHILE")
+		time.sleep(PAUSE_TIME_SEG)
 
 
 def callback(channel):
-    response = post_noise_log(Settings.DEVICE_ID)
-    Log.write("Sound Detected! Channel: " + str(channel) + " - Response: " + str(response))
-    # GPIO.cleanup()
+	print("CALLBACK")
+	response = post_noise_log(Settings.DEVICE_ID)
+	Log.write("Sound Detected! Channel: " + str(channel) + " - Response: " + str(response))
+	# GPIO.cleanup()
