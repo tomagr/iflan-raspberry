c7e511aec6945ba9a73ac22ffe34c6b9f5f26e1e
Tomas Agrimbau <tomas@amalgama.co>
2020-09-16 18:09:55 -0300

    Change log level

--- external/logs/logger.py
+++ external/logs/logger.py
@@ -4,14 +4,17 @@ import logging
 
 class Log:
     def __init__(self):
-        logging.basicConfig(filename=Settings.LOG_FILE,
-                            filemode='a',
-                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                            datefmt='%H:%M:%S',
-                            level=logging.DEBUG)
+        logging.basicConfig(
+            filename=Settings.LOG_FILE,
+            filemode='a',
+            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+            datefmt='%H:%M:%S',
+            level=logging.WARNING
+            # level=logging.DEBUG
+        )
 
     @classmethod
     def write(self, message):
         self.__init__(self)
-        logging.info(message)
+        logging.warning(message) #should be the same as 'level' in config
         return "Writeen!"
